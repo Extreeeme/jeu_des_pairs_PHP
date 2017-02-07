37e52b07358b2e1cfcd700e7ae6c1a86403cd6be
Extreeeme <martindcontact@gmail.com>
2017-02-07 14:28:56 +0100

    Amélioration de la demande

--- index.php
+++ index.php
@@ -4,6 +4,14 @@ $tab = ["img/ane.jpg", "img/chat.jpg", "img/chien.jpg", "img/lama.jpg", "img/lap
 "img/lapins.jpg", "img/lionne.jpg", "img/ours.jpg"];
 $dos = 'img/dos.png'; // On définie l'image de dos
 shuffle($tab);
+
+if(isset($_GET["pseudo"], $_GET["min"], $_GET["sec"]) && gettype($_GET["pseudo"]) == "string" && gettype($_GET["min"]) == "string" && gettype($_GET["sec"]) == "string"){
+	$victoire = true;
+}
+else{
+	$victoire = false;
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -40,7 +48,7 @@ shuffle($tab);
 			
 		<div id="photo"> 
 		<?php
-			if(isset($_GET["pseudo"], $_GET["min"], $_GET["sec"])){
+			if($victoire == true){
 			echo "<h2>Vous avez gagné, recommencer : <a id='lien' href='./index.php'>RECOMMENCER</a></h2>";
 			}
 			else{
